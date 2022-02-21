2f97ca264a43047256da464b23ea1469057778c6
Venci <venci34@gmail.com>
2022-02-21 12:03:50 +0200

    pizza app added

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,6 @@
 import EventEmitter from "eventemitter3";
+import Card from "./Card";
+import Notification from "./Notification";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -9,6 +11,51 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
+
+    const pizzas = [
+      {
+        type: Card.types.HAWAIIAN,
+        price: 8.99,
+      },
+      {
+        type: Card.types.PEPPERONI,
+        price: 9.99,
+      },
+      {
+        type: Card.types.MARGHERITA,
+        price: 7.99,
+      },
+    ];
+
+    pizzas.forEach((pizza) => {
+      const card = new Card({ ...pizza });
+
+      card.render();
+
+      document.querySelector(".main").appendChild(card.container);
+    });
+
+    var elements = document.getElementsByClassName("card");
+
+    for (var i = 0; i < elements.length; i++) {
+      const notification = new Notification(elements[i]);
+      const type = pizzas[i].type;
+      const price = pizzas[i].price;
+      console.log(pizzas[i]);
+      notification.render({ type, price });
+      elements[i].addEventListener("click", function () {
+        document
+          .querySelector(".notifications")
+          .appendChild(notification.container);
+      });
+      notification.container
+        .querySelector(".delete")
+        .addEventListener("click", function () {
+          document
+            .querySelector(".notifications")
+            .removeChild(notification.container);
+        });
+    }
 
     this.emit(Application.events.READY);
   }
---
+++ src/js/Notification.js
@@ -0,0 +1,40 @@
+import classNames from "classnames";
+import { formatCurrency } from "./utils.js";
+
+export default class Notification {
+  static get types() {
+    return {
+      PEPPERONI: "pepperoni",
+      MARGHERITA: "margherita",
+      HAWAIIAN: "hawaiian",
+    };
+  }
+
+  constructor() {
+    this.container = document.createElement("div");
+    this.container.classList.add("notification-container");
+  }
+
+  render({ type, price }) {
+    const template = `
+<div class="notification ${type} type-${type} ${classNames({
+      "is-danger": type === Notification.types.HAWAIIAN,
+    })}"">
+  <button class="delete"></button>
+  🍕 <span class="type">${type}</span> (<span class="price">${formatCurrency(
+      price
+    )}</span>) has been added to your order.
+</div>
+    `;
+
+    this.container.innerHTML = template;
+  }
+
+  close() {
+    console.log("t");
+  }
+
+  empty() {
+    this.container.innerHTML = "";
+  }
+}
